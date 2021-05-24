3f8d6edd1e1b38c789701e22c0b84c879aa1bff3
Christopher Mayorga <christopher.mayorga2@gmail.com>
2021-05-24 15:52:56 -0500

    Adds logistic regression exercises

--- prepare.py
+++ prepare.py
@@ -50,3 +50,26 @@ def prep_titanic_data(df):
     validate = validate.drop(columns=['sex', 'embark_town'])
     test = test.drop(columns=['sex', 'embark_town'])
     return train, validate, test
+
+def prep_titanic_data_for_logit(df):
+    '''
+    takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned dataframe
+    arguments: df: a pandas DataFrame with the expected feature names and columns
+    return: train, test, split: three dataframes with the cleaning operations performed on them
+    '''
+    df = df.drop_duplicates()
+    df = df.drop(columns=['deck', 'embarked', 'class', 'passenger_id'])
+    df.age = df.age.fillna(value=df.age.mean())
+    train, test = train_test_split(df, test_size=0.2, random_state=1349, stratify=df.survived)
+    train, validate = train_test_split(train, train_size=0.7, random_state=1349, stratify=train.survived)
+    train, validate, test = impute_mode(train, validate, test)
+    dummy_train = pd.get_dummies(train[['sex', 'embark_town']], drop_first=[True,True])
+    dummy_validate = pd.get_dummies(validate[['sex', 'embark_town']], drop_first=[True,True])
+    dummy_test = pd.get_dummies(test[['sex', 'embark_town']], drop_first=[True,True])
+    train = pd.concat([train, dummy_train], axis=1)
+    validate = pd.concat([validate, dummy_validate], axis=1)
+    test = pd.concat([test, dummy_test], axis=1)
+    train = train.drop(columns=['sex', 'embark_town'])
+    validate = validate.drop(columns=['sex', 'embark_town'])
+    test = test.drop(columns=['sex', 'embark_town'])
+    return train, validate, test
